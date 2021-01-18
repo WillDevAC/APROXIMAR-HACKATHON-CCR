4b040f4a82ac83cfcac16056f2c31e3c2f9da802
WillDevAC <wydemairomsilva@gmail.com>
2021-01-17 23:38:21 -0300

    Update email

---
+++ assets/js/email.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+	$('#enviaremail').on('click', function(){
+		$("#msg").html("<p style='color: pink'>Parabens!!! Em breve iremos entrar em contato com você =D<p>");
+	})
+})
+
